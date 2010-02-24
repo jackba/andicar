85f16b1c05cce425b1ac5e20b3964828a4ce5c79
miki <miki@miki-home>
2010-02-24 21:30:04 +0200

    build release template sh

--- buildReleaseTemplate.sh
+++ buildReleaseTemplate.sh
@@ -2,7 +2,7 @@
 rm *.apk
 ant release
 cp ./dist/AndiCar.apk ./AndiCarUnsigned.apk
-jarsigner -verbose -keystore ../<keystorefile> AndiCarUnsigned.apk <keystore password>
+jarsigner -verbose -keystore ../<keystorefile> AndiCarUnsigned.apk <keystore alias>
 <android-sdk>/tools/zipalign -v 4 AndiCarUnsigned.apk AndiCar.apk
 
 
