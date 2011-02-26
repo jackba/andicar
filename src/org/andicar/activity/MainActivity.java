a7318e88bda9c918cdc219ee712486218a42207a
miki <miki@miki-home>
2011-02-26 20:07:32 +0200

    NI

--- src/org/andicar/activity/MainActivity.java
+++ src/org/andicar/activity/MainActivity.java
@@ -365,9 +365,9 @@ public class MainActivity extends BaseActivity {
 //			}
 
 			//debug
-//			Intent intent = new Intent(this, ToDoNotificationService.class);
-//			intent.putExtra("ToDoID", 77L);
-//			this.startService(intent);
+			Intent intent = new Intent(this, ToDoNotificationService.class);
+//			intent.putExtra("ToDoID", 22L);
+			this.startService(intent);
 			
 			lastTime = mPreferences.getLong("lastAddOnCheckTime", 0);
 			if(isJustInstalled){
