da0d6d11b7792e7b86bfd18181d4b6671e79a3a6
miki <miki@miki-home>
2011-04-05 21:37:21 +0300

    359db

--- src/org/andicar/utils/StaticValues.java
+++ src/org/andicar/utils/StaticValues.java
@@ -30,7 +30,7 @@ import android.os.Environment;
 public class StaticValues
 {
     public static final int DB_URI_DRIVERS = 1;
-    public static final int DATABASE_VERSION = 358;
+    public static final int DATABASE_VERSION = 359;
     public static final String DATABASE_NAME = "AndiCar.db";
     public static final String GLOBAL_PREFERENCE_NAME = "AndiCarPreferences";
     /*
