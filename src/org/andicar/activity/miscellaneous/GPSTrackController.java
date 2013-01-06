a35b59079ac02f08f9e8a8da29594c751cf404e6
miki <miki@miki-home>
2013-01-06 15:50:23 +0200

    NI

--- src/org/andicar/activity/miscellaneous/GPSTrackController.java
+++ src/org/andicar/activity/miscellaneous/GPSTrackController.java
@@ -1,16 +1,20 @@
 /*
-    Copyright (C) 2009-2010 Miklos Keresztes - miklos.keresztes@gmail.com
-
-    This program is free software; you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by the
-    Free Software Foundation; either version 2 of the License.
-
-    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-    without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-    See the GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License along with this program;
-    if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *  AndiCar - a car management software for Android powered devices.
+ *
+ *  Copyright (C) 2013 Miklos Keresztes (miklos.keresztes@gmail.com)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.andicar.activity.miscellaneous;
